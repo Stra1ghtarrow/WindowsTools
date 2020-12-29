a10cbb8ae1af469464d0e8951eedc5dbc1c4d762
Stra1ghtarrow <63554445+Stra1ghtarrow@users.noreply.github.com>
2020-12-29 15:06:36 +0000

    Add files via upload

---
+++ Displayname_to_Samaccountname.ps1
@@ -0,0 +1,12 @@
+﻿$displayname = @()
+$names = get-content "c:\gts\enabled.txt"
+foreach ($name in $names) {
+
+
+$displaynamedetails = Get-ADUser -filter { DisplayName -eq $name } -server "ServerNamegoeshere"| Select name,samAccountName
+
+$displayname += $displaynamedetails
+
+}
+
+$displayname | Export-Csv "C:\gts\Samaccountname.csv"
---
+++ showpendingupdates.ps1
@@ -0,0 +1,31 @@
+﻿$msUpdateSession = [activator]::CreateInstance([type]::GetTypeFromProgID("Microsoft.Update.Session",$env:COMPUTERNAME))
+$updates = $msUpdateSession.CreateUpdateSearcher().Search("IsInstalled=0").Updates
+
+$silverLightInstalled = Get-WindowsFeature | where { $_.Name.ToLower() -like 'silverlight' }
+
+$pendingUpdates = [System.Collections.ArrayList]@()
+
+$updates | % {
+
+$title = $_.Title
+
+if ($_.Title.ToLower().Contains("silverlight")){
+    if($silverLightInstalled){
+        $pendingUpdates.Add($title)}
+}
+
+else{
+        $pendingUpdates.Add($title)
+    }
+}
+
+$updatesCount = $pendingUpdates.Count
+
+if($updatesCount -gt 0){
+    Write-Host "***** $updatesCount updates pending for this machine*****" -BackgroundColor Black -ForegroundColor Yellow
+    $pendingUpdates | % { Write-Host $_}
+}
+
+else{
+    Write-Host "***** No pending updates for $env:COMPUTERNAME*****" -BackgroundColor Black -ForegroundColor Green
+}
